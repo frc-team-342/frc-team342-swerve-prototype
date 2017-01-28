965abe96d3d158fad2b09d716898d9100fe7aee0
Elizabeth O'Neill <elizabethoneill5678@gmail.com>
2017-01-28 15:36:18 -0500

    changed angle and magnitude so that values greter than one equal one

--- src/org/usfirst/frc/team342/swerve_prototype/RobotMap.java
+++ src/org/usfirst/frc/team342/swerve_prototype/RobotMap.java
@@ -6,9 +6,6 @@ package org.usfirst.frc.team342.swerve_prototype;
  * floating around.
  */
 public class RobotMap {
-	public static final int Servo1 = 1;
-	public static final int Servo2 = 2;
-	
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
@@ -18,4 +15,15 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+
+public static final int frdrive=1;
+public static final int brdrive=2;
+public static final int bldrive=3;
+public static final int fldrive=4;
+public static final int frturn=5;
+public static final int brturn=6;
+public static final int blturn=7;
+public static final int flturn=8;
+
+
 }
